10ead1decbb54d843634dd165113657b7e86fa4f
James Cleary <jcleary05@qub.ac.uk>
2018-09-08 00:02:46 +0100

    EFR-1: First Commit, Initial Set Up

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Welcome to EFR MMA!</title>
+  </head>
+  <body>
+  <!-- Nav Bar -->
+  <div class="container">
+  	<ul class="nav justify-content-end">
+  		<li class="nav-item">
+  			<a href="#" class="nav-link active">Home</a>
+  		</li>
+  		<li class="nav-item">
+  			<a href="#" class="nav-link">About Us</a>
+  		</li>
+  		<li class="nav-item">
+  			<a href="#" class="nav-link ">Timetable</a>
+  		</li>
+  		<li class="nav-item">
+  			<a href="#" class="nav-link">Blog</a>
+  		</li>
+  		<li class="nav-item">
+  			<a href="#" class="nav-link">Contact Us</a>
+  		</li>
+  	</ul>
+  </div>
+
+  <!-- Headings -->
+  <div class="container">
+    <h1>Welcome To EFR Mixed Martial Arts</h1>
+    <h2>Irelands Longest Running MMA GYM</h2>
+  </div>
+
+  <!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+      <!-- Grid row -->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+          <!-- Content -->
+          <h5 class="text-uppercase">Footer Content</h5>
+          <p>Here you can use rows and columns here to organize your footer content.</p>
+        </div>
+        <!-- Grid column -->
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  </body>
+</html>
